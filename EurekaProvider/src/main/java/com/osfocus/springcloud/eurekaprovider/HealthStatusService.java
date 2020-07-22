8587c3e3611270f4c1a4ec71ff1a7646feebcd82
Adrian LIU <adrian.liu.work@gmail.com>
2020-07-22 10:22:15 +1000

    Added: Health Update and security

---
+++ EurekaProvider/src/main/java/com/osfocus/springcloud/eurekaprovider/HealthStatusService.java
@@ -0,0 +1,27 @@
+package com.osfocus.springcloud.eurekaprovider;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HealthStatusService implements HealthIndicator {
+    private Boolean status = true;
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public Health health() {
+        if (status) {
+            return new Health.Builder().up().build();
+        } else {
+            return new Health.Builder().down().build();
+        }
+    }
+}
--- EurekaProvider/src/main/java/com/osfocus/springcloud/eurekaprovider/MainController.java
+++ EurekaProvider/src/main/java/com/osfocus/springcloud/eurekaprovider/MainController.java
@@ -1,12 +1,23 @@
 package com.osfocus.springcloud.eurekaprovider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @Autowired
+    HealthStatusService hsrv;
+
     @GetMapping("/hi")
     public Object getHi() {
         return "hi";
+    }
+
+    @GetMapping("/health")
+    public String health(@RequestParam("status") Boolean status) {
+        hsrv.setStatus(status);
+        return String.valueOf(hsrv.getStatus());
     }
 }

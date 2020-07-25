3f6242d944d22cd272c36d90fb6e5e50d8257e4a
Adrian LIU <adrian.liu.work@gmail.com>
2020-07-25 22:48:47 +1000

    Changed: use OpenFeign connected with Eureka to get data from service provider

--- UserConsumer/src/main/java/com/osfocus/springcloud/userconsumer/UserApi.java
+++ UserConsumer/src/main/java/com/osfocus/springcloud/userconsumer/UserApi.java
@@ -3,7 +3,7 @@ package com.osfocus.springcloud.userconsumer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "user-provider", url = "http://localhost:8083")
+@FeignClient(name = "user-provider")
 public interface UserApi {
     @GetMapping("/alive")
     public String alive();

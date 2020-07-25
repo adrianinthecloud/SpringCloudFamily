738708beca9991870683da4a3506a3527d1a2152
Adrian LIU <adrian.liu.work@gmail.com>
2020-07-25 22:45:17 +1000

    Added: use OpenFeign to get data from provider using url

---
+++ UserProvider/src/test/java/com/osfocus/springcloud/userprovider/UserProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osfocus.springcloud.userprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

86860636694cdcd4c5a7310e064f7f4c0b12c130
Adrian LIU <adrian.liu.work@gmail.com>
2020-07-30 20:49:09 +1000

    Changed: Extended and added more method to the ConsumerApi and ProviderApi

--- UserConsumer/src/main/java/com/osfocus/springcloud/userconsumer/MainController.java
+++ UserConsumer/src/main/java/com/osfocus/springcloud/userconsumer/MainController.java
@@ -1,8 +1,8 @@
 package com.osfocus.springcloud.userconsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
 public class MainController {
@@ -13,4 +13,33 @@ public class MainController {
     public Object getAlive() {
         return api.alive();
     }
+
+    @GetMapping("/map")
+    public Map<Integer, String> map(Integer id) {
+        System.out.println(id);
+        return api.getMap(id);
+    }
+
+    @GetMapping("/map2")
+    public Map<Integer, String> map2(Integer id,String name) {
+        System.out.println(id);
+        return api.getMap2(id,name);
+    }
+
+
+    @GetMapping("/map3")
+    public Map<Integer, String> map3(@RequestParam Map<String, Object> map) {
+
+        System.out.println(map);
+        return api.getMap3(map);
+    }
+
+
+    @GetMapping("/map4")
+    public Map<Integer, String> map4(@RequestParam Map<String, Object> map) {
+
+        System.out.println(map);
+        return api.postMap(map);
+    }
+
 }
--- UserProvider/src/main/java/com/osfocus/springcloud/userprovider/MainController.java
+++ UserProvider/src/main/java/com/osfocus/springcloud/userprovider/MainController.java
@@ -1,13 +1,43 @@
 package com.osfocus.springcloud.userprovider;
 
 import com.osfocus.userapi.UserApi;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class MainController implements UserApi {
-    @GetMapping("/alive")
+    @Override
     public String alive() {
         return "ok test";
+    }
+
+    @GetMapping("/getMap")
+    public Map<Integer, String> getMap(@RequestParam("id") Integer id) {
+        // TODO Auto-generated method stub
+        System.out.println(id);
+        return Collections.singletonMap(id, "mmeme");
+    }
+    @GetMapping("/getMap2")
+    public Map<Integer, String> getMap2(Integer id,String name) {
+        // TODO Auto-generated method stub
+        System.out.println(id);
+        return Collections.singletonMap(id, name);
+    }
+
+    @GetMapping("/getMap3")
+    public Map<Integer, String> getMap3(@RequestParam Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        System.out.println(map);
+        return Collections.singletonMap(Integer.parseInt(map.get("id").toString()), map.get("name").toString());
+    }
+
+
+    @PostMapping("/postMap")
+    public Map<Integer, String> postMap(@RequestBody Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        System.out.println(map);
+        return Collections.singletonMap(Integer.parseInt(map.get("id").toString()), map.get("name").toString());
     }
 }
